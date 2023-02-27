7da5177afa2c78697c94353baff3fd9248811b4a
klimushkin1942 <muhammed1942ali@gmail.com>
2023-02-27 14:14:51 +0300

    error correction

---
+++ app/Enums/RoleTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+use App\Models\Role;
+enum RoleTypes: string
+{
+    case ADMIN = '1';
+    case USER = '2';
+    case OBSERVER = '3';
+    public function type(): string
+    {
+        return $this->value;
+    }
+    public function id()
+    {
+        return Role::where('name', self::type())->first()->id;
+    }
+}
